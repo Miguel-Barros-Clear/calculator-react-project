89d9c5ef8ef6060af736e5d131387b970e42b0f3
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-29 08:20:09 -0300

    feat: Creating the buttons component for the calculator

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,8 +1,52 @@
 import React, { Component } from "react";
 import "./Calculator.css";
 
+import Button from "../components/Button";
+import Display from "../components/Display";
+
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.setDigit = this.setDigit.bind(this);
+  }
+
+  clearMemory() {
+    console.log("");
+  }
+
+  setOperation(operation) {
+    console.log("");
+  }
+
+  setDigit(n) {
+    console.log("");
+  }
+
   render() {
-    return <div className="calculator"></div>;
+    return (
+      <div className="calculator">
+        <Display value={100} />
+        <Button label="AC" click={this.clearMemory} triple />
+        <Button label="/" click={this.setOperation} operation />
+        <Button label="7" click={this.setDigit} />
+        <Button label="8" click={this.setDigit} />
+        <Button label="9" click={this.setDigit} />
+        <Button label="*" click={this.setOperation} operation />
+        <Button label="4" click={this.setDigit} />
+        <Button label="5" click={this.setDigit} />
+        <Button label="6" click={this.setDigit} />
+        <Button label="-" click={this.setOperation} operation />
+        <Button label="1" click={this.setDigit} />
+        <Button label="2" click={this.setDigit} />
+        <Button label="3" click={this.setDigit} />
+        <Button label="+" click={this.setOperation} operation />
+        <Button label="0" click={this.setDigit} double />
+        <Button label="." click={this.setDigit} />
+        <Button label="=" click={this.setOperation} operation />
+      </div>
+    );
   }
 }
